eb7c350fdb6dee4c93c868bae36adbaf4b955b7a
YoshuaNava <yoshua.nava.chocron@gmail.com>
2017-09-04 18:31:30 +0200

    Updating launch files, taking out joint remappers and renaming some files

--- yumi_hw/scripts/gazebo_grippers_remap.py
+++ yumi_hw/scripts/gazebo_grippers_remap.py
@@ -12,8 +12,6 @@ import threading
 grippers_indices = [0, 1]
 left_arm_indices = [0, 2, 4, 6, 8, 10, 12, 14]
 right_arm_indices = [1, 3, 5, 7, 9, 11, 13, 15]
-right_arm_pub = None
-left_arm_pub = None
 
 grippers_state_pub = None
 gripper_r_cmd_subs = None
@@ -28,29 +26,11 @@ gripper_srv_force = 5.0
 
 
 def joint_states_callback(data):
-    global right_arm_pub, left_arm_pub, grippers_indices, left_arm_indices, right_arm_indices
+    global grippers_indices, grippers_state_pub
     
-    left_arm_msg = JointState()
-    left_arm_msg.header.stamp = rospy.Time.now()
-    left_arm_msg.header.frame_id = '/world'
-    left_arm_msg.name = [data.name[i] for i in left_arm_indices]
-    left_arm_msg.position = [data.position[i] for i in left_arm_indices]
-    left_arm_msg.velocity = [data.velocity[i] for i in left_arm_indices]
-    left_arm_msg.effort = [data.effort[i] for i in left_arm_indices]
-    left_arm_pub.publish(left_arm_msg)
-
-    right_arm_msg = JointState()
-    right_arm_msg.header.stamp = rospy.Time.now()
-    right_arm_msg.header.frame_id = '/world'
-    right_arm_msg.name = [data.name[i] for i in right_arm_indices]
-    right_arm_msg.position = [data.position[i] for i in right_arm_indices]
-    right_arm_msg.velocity = [data.velocity[i] for i in right_arm_indices]
-    right_arm_msg.effort = [data.effort[i] for i in right_arm_indices]
-    right_arm_pub.publish(right_arm_msg)
-
     grippers_state_msg = JointState()
     grippers_state_msg.header.stamp = rospy.Time.now()
-    grippers_state_msg.header.frame_id = '/world'
+    grippers_state_msg.header.frame_id = '/yumi_base_link'
     grippers_state_msg.name = [data.name[i] for i in grippers_indices]
     grippers_state_msg.position = [data.position[i] for i in grippers_indices]
     grippers_state_pub.publish(grippers_state_msg)
@@ -96,11 +76,9 @@ def request_release_handle(req):
 
 
 def listener():
-    global right_arm_pub, left_arm_pub, grippers_state_pub, gripper_l_cmd_subs, gripper_l_cmd_pub, gripper_r_cmd_subs, gripper_r_cmd_pub, gripper_dograsp_service
+    global grippers_state_pub, gripper_l_cmd_subs, gripper_l_cmd_pub, gripper_r_cmd_subs, gripper_r_cmd_pub, gripper_dograsp_service
     rospy.init_node('gazebo_joints_remap', anonymous=False)
     rospy.Subscriber("/yumi/joint_states", JointState, joint_states_callback)
-    left_arm_pub = rospy.Publisher("/joint_states", JointState, queue_size=1)
-    right_arm_pub = rospy.Publisher("/joint_states", JointState, queue_size=1)
 
     grippers_state_pub = rospy.Publisher("/yumi/gripper_states", JointState, queue_size=1)
     gripper_l_cmd_pub = rospy.Publisher("/yumi/gripper_effort_controller_l/command", Float64, queue_size=1)
@@ -111,6 +89,8 @@ def listener():
 
     gripper_dograsp_service = rospy.Service('/yumi/yumi_gripper/do_grasp', YumiGrasp, request_grasp_handle)
     gripper_releasegrasp_service = rospy.Service('/yumi/yumi_gripper/release_grasp', YumiGrasp, request_release_handle)
+
+    rospy.loginfo("\x1b[6;30;42m" + "Grippers remapper started. Now you can start grasping objects in simulation!" + "\x1b[0m")
 
     rospy.spin()
 
